abb46f48c4b89421b47e90f436f0856387d983d5
yehtetsan1 <yehtetsan.zotefamily@gmail.com>
2025-01-18 20:21:06 +0630

    add download button

---
+++ assets/js/button.js
@@ -0,0 +1,10 @@
+document.querySelector('.download-btn').addEventListener('click', function(e) {
+  e.preventDefault();
+  var link = document.createElement('a');
+  link.href = '../assets/Files/HtetHtetOoLwinResume.docx';
+  link.setAttribute('download', '');
+  link.style.display = 'none';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
