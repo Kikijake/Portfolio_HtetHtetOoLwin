99e23b856d4955531cdce75e69b1c4e2db06ff96
yehtetsan1 <yehtetsan.zotefamily@gmail.com>
2025-01-18 20:35:41 +0630

    modify docx to pdf

--- assets/js/button.js
+++ assets/js/button.js
@@ -1,7 +1,7 @@
 document.querySelector('.download-btn').addEventListener('click', function(e) {
   e.preventDefault();
   var link = document.createElement('a');
-  link.href = '../assets/Files/HtetHtetOoLwinResume.docx';
+  link.href = '../assets/Files/HtetHtetOoLwinResume.pdf';
   link.setAttribute('download', '');
   link.style.display = 'none';
   document.body.appendChild(link);
